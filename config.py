8c35a7a99b2f86c047eb02a87bef679788108fb1
AnimeFactory <166727007+AnimeFactory@users.noreply.github.com>
2024-07-13 16:35:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,16 +46,16 @@ DB_NAME = environ.get("DB_NAME", "vjbotz")
 
 # Bot Information
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-BOT_USERNAME = environ.get("BOT_USERNAME", "Bulla_The_File_Store_Bot") # your bot username without @
-PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
+BOT_USERNAME = environ.get("BOT_USERNAME", "BulmaTheFileStoreBot") # your bot username without @
+PICS = (environ.get('PICS', 'https://te.legra.ph/file/1f7fbd2bd8a674b42db3a.jpg https://te.legra.ph/file/b458d921a12b273d35a53.jpg https://te.legra.ph/file/5b0c2f8fc9adfc24ea6d2.jpg https://te.legra.ph/file/8df5b8ba947a2485d22d5.jpg https://te.legra.ph/file/eeb53c14e3487afbb8915.jpg https://te.legra.ph/file/a2be48e58a08d202e5030.jpg https://te.legra.ph/file/e130e2a1d638991560f1d.jpg https://te.legra.ph/file/ba06d9068a53eb895eb04.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "10")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "600")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002042077879"))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002042077879')).split()]
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002177501438"))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002210146714')).split()]
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
@@ -79,7 +79,7 @@ class Var(object):
     name = str(getenv('name', 'filetolinkvjbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002042077879'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002177501438'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
